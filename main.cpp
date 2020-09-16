a35f2a8b53b35304961ed63f9398493c624d8b1c
Gael <gael.henry.sae@gmail.com>
2020-09-16 12:18:22 +0200

    first time code day 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
-#include <windows.h>
+#include<iostream>
 
-int main()
+int main (/*int ac, char** av*/)
 {
-	MessageBox(nullptr, "World!", "Hello", MB_ICONEXCLAMATION);
+	/*int ecrit;
+	//this is how you can output something ont the console !
+	std::cout << "Hello world!";
+	std:: cout << " ";
+	std::cout << "je te pisse dessus !\n";
+
+	std::cout << "ecrit un nombre:\n";
+	std::cin >> ecrit;
+	std::cout << "tu as ecrit " << ecrit << "\n";
+	system ("exit");
+
+	int a = 3;
+	int b = 2;
+	int c = 0;
+
+	c = a + b;
+	std::cout << c << "\n";
+	c = a - b;
+	std::cout << c << "\n";
+	c = a * b;
+	std::cout << c << "\n";
+	c = a / b;
+	std::cout << c << "\n";
+	c = c + 1;
+	std::cout << c << "\n";
+	c = a - c * b;
+	std::cout << c << "\n";
+	return c;*/
+
+
+
 }
